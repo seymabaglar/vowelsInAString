3bafbdfd0d57d810ac18059899e8845ad7e29afc
seyma <symbglr54@gmail.com>
2023-06-08 15:24:02 +0300

    son

--- java.js
+++ java.js
@@ -1,7 +1,7 @@
 function countVowels(){
     let str = document.getElementById('inputString').value;
    
-    let matches = str.match(/[aeiou]/gi);
+    let matches = str.match(/[aeiıouüö]/gi);
     let count = matches ? matches.length : 0;
     document.getElementById('result').innerHTML = "There are " + count + " vowels in  <small style='color:red; font-weight:600; font-size:2rem'>" + str + "</small>" ;
 
